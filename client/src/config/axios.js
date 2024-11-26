00a3c5dab873ca510651f7060e3a7a5b105918cf
Isayah <118870380+Taiyou0501@users.noreply.github.com>
2024-11-26 23:32:12 +0800

    axiox

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseURL = import.meta.env.MODE === 'development' 
     ? 'http://localhost:8081'
-    : 'https://ereport-4.onrender.com';
+    : import.meta.env.VITE_API_URL;
 
 console.log('Mode:', import.meta.env.MODE);
 console.log('Base URL:', baseURL);
